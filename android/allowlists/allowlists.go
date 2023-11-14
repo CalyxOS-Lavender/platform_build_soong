699039e5a78a08a7dd038fd5d4eb736c4c6554b7
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:30:22 -0500

    allowlists: support inline signing for U [1/2]
    For those who have used .android-certs previously, cp .android-certs/* vendor/calyx/signing/keys/
    
    Change-Id: If8e94c61d473ca150631b7423655d552f266228f
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- android/allowlists/allowlists.go
+++ android/allowlists/allowlists.go
@@ -14,7 +14,30 @@
 
 package allowlists
 
+// Configuration to decide if modules in a directory should default to true/false for bp1build_available
+type Bp1BuildConfig map[string]BazelConversionConfigEntry
+type BazelConversionConfigEntry int
+
 const (
+	// iota + 1 ensures that the int value is not 0 when used in the Bp2buildAllowlist map,
+	// which can also mean that the key doesn't exist in a lookup.
+
+	// all modules in this package and subpackages default to bp2build_available: true.
+	// allows modules to opt-out.
+	Bp1BuildDefaultTrueRecursively BazelConversionConfigEntry = iota + 1
+
+	// all modules in this package (not recursively) default to bp1build_available: true.
+	// allows modules to opt-out.
+	Bp1BuildDefaultTrue
+
+	// all modules in this package (not recursively) default to bp2build_available: false.
+	// allows modules to opt-in.
+	Bp1BuildDefaultFalse
+
+	// all modules in this package and subpackages default to bp2build_available: false.
+	// allows modules to opt-in.
+	Bp1BuildDefaultFalseRecursively
+
 	// Modules with build time of more than half a minute should have high priority.
 	DEFAULT_PRIORITIZED_WEIGHT = 1000
 	// Modules with build time of more than a few minute should have higher priority.
@@ -32,5 +55,16 @@ var (
 		"droidstubs":  DEFAULT_PRIORITIZED_WEIGHT,
 		"art_":        DEFAULT_PRIORITIZED_WEIGHT,
 		"ndk_library": DEFAULT_PRIORITIZED_WEIGHT,
+	}
+
+	Bp1buildDefaultConfig = Bp1BuildConfig{
+		"vendor/calyx/signing/keys":           Bp1BuildDefaultTrue,
+	}
+
+	Bp1buildKeepExistingBuildFile = map[string]bool{
+		// This is actually build/bazel/build.BAZEL symlinked to ./BUILD
+		".":/*recursive = */ false,
+
+		"vendor/calyx/signing/keys":/* recursive = */ false,
 	}
 )
